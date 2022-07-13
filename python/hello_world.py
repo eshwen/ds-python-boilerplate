a6866e73551d388ac664fb8f4310da4167adbe1c
Eshwen Bhal <eshwen.bhal@huboo.com>
2022-07-13 13:51:25 +0100

    Test module

---
+++ python/hello_world.py
@@ -0,0 +1,10 @@
+"""Test file."""
+
+def hello_world() -> str:
+    """Returns "Hello World!" when called.
+
+    Returns:
+        str: The string 'Hello world!'.
+
+    """
+    return "Hello World!"
