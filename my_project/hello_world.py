9d31ca31f869805308d8bad9b6a98e7911ca945b
Eshwen Bhal <milanman.eshb@gmail.com>
2022-09-23 11:42:59 +0100

    Add example block

--- my_project/hello_world.py
+++ my_project/hello_world.py
@@ -7,5 +7,17 @@ def hello_world() -> str:
     Returns:
         str: The string 'Hello world!'.
 
+    Example:
+        ```python
+        >>> hello_world()
+        'Hello World!'
+        ```
+
+    Note:
+        This is an example of a note.
+
+    Warning:
+        This is an example of a warning.
+
     """
     return "Hello World!"
