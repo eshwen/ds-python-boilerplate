9cb9522c3e604a32c4a44c36c03e836b5740907e
Eshwen Bhal, PhD <milanman.eshb@gmail.com>
2022-11-13 19:45:24 +0000

    Replace mypy script with command

--- scripts/type-check-code.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source $SCRIPT_DIR/resolve-dirs.sh
-
-mypy my_project/ --ignore-missing-imports
